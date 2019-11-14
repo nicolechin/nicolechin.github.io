ea4a54c1fa0d744ba65765e23af9991870f768a8
Nicole Chin <nicolec.melbourne@gmail.com>
2019-11-14 16:37:09 +1100

    Update

---
+++ community page/assets/js/main.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $('.close').click(function() {
+	$('.paper').removeClass('active');
+	});
+
+
+});
+
+
+
+
--- test-homepage/assets/js/main.js
+++ test-homepage/assets/js/main.js
@@ -30,3 +30,67 @@ $('.close').click(function() {
 $('.postcard-7').click(function() {
 	$('.full-7').addClass('active');
 });
+
+$('.close').click(function() {
+	$('.full').removeClass('active');
+});
+
+
+$('.postcard-8').click(function() {
+	$('.full-8').addClass('active');
+});
+
+$('.close').click(function() {
+	$('.full').removeClass('active');
+});
+
+
+$('.postcard-6').click(function() {
+	$('.full-6').addClass('active');
+});
+
+$('.close').click(function() {
+	$('.full').removeClass('active');
+});
+
+
+$('.postcard-5').click(function() {
+	$('.full-5').addClass('active');
+});
+
+$('.close').click(function() {
+	$('.full').removeClass('active');
+});
+
+
+$('.postcard-4').click(function() {
+	$('.full-4').addClass('active');
+});
+
+$('.close').click(function() {
+	$('.full').removeClass('active');
+});
+
+
+$('.postcard-2').click(function() {
+	$('.full-2').addClass('active');
+});
+
+$('.close').click(function() {
+	$('.full').removeClass('active');
+});
+
+
+$('.postcard-3').click(function() {
+	$('.full-3').addClass('active');
+});
+
+$('.close').click(function() {
+	$('.full').removeClass('active');
+});
+
+
+$('.postcard-10').click(function() {
+	$('.full-10').addClass('active');
+});
+
